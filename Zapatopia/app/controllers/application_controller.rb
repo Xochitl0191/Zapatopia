241d7ab495ba9300c1ce4238cca6bd098c5b9144
Xochitl0191 <xochitlfm@gmail.com>
2017-11-21 16:03:49 -0800

    controllers and partials added for carts, oders, shoes,

--- Zapatopia/app/controllers/application_controller.rb
+++ Zapatopia/app/controllers/application_controller.rb
@@ -1,3 +1,17 @@
-class ApplicationController < ActionController::Base
+class ApplicationController < ApplicationRecord
   protect_from_forgery with: :exception
+ helper_method :current_order
+
+ def current_order
+   if !session[:order_id].nil?
+     Order.find(session[:order_id])
+   else
+     Order.new
+   end
+
+ end
+ private
+ def  protect_from_forgery
+ end
+
 end
